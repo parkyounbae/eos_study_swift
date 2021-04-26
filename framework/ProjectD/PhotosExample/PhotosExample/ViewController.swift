1a353c2f8b31a32d8ee4d1c7749bf760beba5c35
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-04-26 15:07:56 +0900

    사진첩에서 사진 가져오기

--- framework/ProjectD/PhotosExample/PhotosExample/ViewController.swift
+++ framework/ProjectD/PhotosExample/PhotosExample/ViewController.swift
@@ -17,19 +17,71 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func requestCollection(){
         let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        //사진 찍으면 저장되는 그 카메라 롤을 가져옴
         
         guard let cameraRollCollection = cameraRoll.firstObject else {
             return
         }
         
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+        let fetchOptions = PHFetchOptions() //사진들을 가져오고
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)] //최신순으로 정렬
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions) //그 결과를 저 프로퍼티에 저장
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //뷰를 불러온 다음에 사진에 대한 접근 허가 여부를 판단한다.
+        let photoAurhorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAurhorizationStatus {
+        case .authorized:
+            print("접근 허가")
+            self.requestCollection()
+            self.tableView.reloadData()
+        case .denied:
+            print("접근 불가")
+        case .notDetermined:
+            print("응답 없음")
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status {
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    
+                    OperationQueue.main.addOperation {
+                        self.tableView.reloadData()
+                    }
+                case .denied:
+                    print("사용자가 거절함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        case .limited:
+            print("limited")
+        @unknown default:
+            fatalError()
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchResult?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+        //index에 해당하는 사진을
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView?.image = image})
+        //이미지 매니저를 통해 이미지를 요청함. 셀에 들어갈 사이즈로 작게, 가져온 뒤 셀에 이미지 넣어줌
+        
+        return cell
     }
 
 
