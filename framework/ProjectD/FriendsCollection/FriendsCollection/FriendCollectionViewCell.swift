0732da63a45c7994a091f3f68b2ecdc119328772
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-04-28 13:09:15 +0900

    셀 커스터마이즈

---
+++ framework/ProjectD/FriendsCollection/FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by 박윤배 on 2021/04/28.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+}
--- framework/ProjectD/FriendsCollection/FriendsCollection/ViewController.swift
+++ framework/ProjectD/FriendsCollection/FriendsCollection/ViewController.swift
@@ -9,22 +9,28 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    var numberOfCell: Int = 10
     let cellIdentifier: String = "cell"
+    var friends: [Friend] = []
+    @IBOutlet weak var collectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.numberOfCell
+        return self.friends.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        let cell: FriendCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! FriendCollectionViewCell
+        
+        let friend: Friend = self.friends[indexPath.item]
+        
+        cell.nameAgeLabel.text = friend.nameAndAge
+        cell.adressLabel.text = friend.fulladress
         
         return cell
     }
     
     //셀렉션 뷰에 어떤 아이템을 선택하게 되면 셀의 수를 늘려줌
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.numberOfCell += 1
+        //self.numberOfCell += 1
         collectionView.reloadData() //한개를 늘려주고 데이터 리로드
     }
     
