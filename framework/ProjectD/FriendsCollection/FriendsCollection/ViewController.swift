5f373e4e1567a65da4315cc3ad310aa8fccc6869
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-04-28 14:03:57 +0900

    셀 크기 조절

--- framework/ProjectD/FriendsCollection/FriendsCollection/ViewController.swift
+++ framework/ProjectD/FriendsCollection/FriendsCollection/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         let friend: Friend = self.friends[indexPath.item]
         
         cell.nameAgeLabel.text = friend.nameAndAge
-        cell.adressLabel.text = friend.fulladress
+        cell.adressLabel.text = friend.fullAddress
         
         return cell
     }
@@ -38,6 +38,32 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let flowLayout: UICollectionViewFlowLayout
+        flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10) //셀 내의 섹션의 인셋을 0으로
+        flowLayout.minimumInteritemSpacing = 10 //아이템간의 거리가 10보다는 크게
+        flowLayout.minimumLineSpacing = 10 //줄 간의 거리가 10보다는 크게
+        
+        let halfWidth: CGFloat = UIScreen.main.bounds.width / 2.0 //화면에 2개씩 배치
+        
+        flowLayout.itemSize = CGSize(width: halfWidth - 30, height: 90)
+        //예상사이즈 지정 -> 오토레이아웃에 따라 셀 사이즈가 변할 수 있기 때문
+        
+        self.collectionView.collectionViewLayout = flowLayout
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "friends") else {
+            return
+        }
+        
+        do{
+            self.friends = try jsonDecoder.decode([Friend].self, from: dataAsset.data)
+        } catch{
+            print(error.localizedDescription)
+        }
+        
+        self.collectionView.reloadData()
     }
 
 
