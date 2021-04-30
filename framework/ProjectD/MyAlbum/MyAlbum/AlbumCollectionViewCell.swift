a707d1808076a4ac9d71745bda5490c246556512
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-04-30 16:19:06 +0900

    maincontrollerView

---
+++ framework/ProjectD/MyAlbum/MyAlbum/AlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by 박윤배 on 2021/04/30.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var albumNameLabel: UILabel!
+    @IBOutlet var albumCountLabel: UILabel!
+    @IBOutlet var albumThumImageView: UIImageView!
+}
--- framework/ProjectD/MyAlbum/MyAlbum/ViewController.swift
+++ framework/ProjectD/MyAlbum/MyAlbum/ViewController.swift
@@ -6,12 +6,95 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var albumCollectionView: UICollectionView!
+    var albums: [PHAssetCollection] = []
+    var fetchResult: PHFetchResult<PHAsset>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    let cellIdentifier: String = "cell"
+    
+    func requestCollection(){
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        let albumThumnail: PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .smartAlbumUserLibrary, options: fetchOptions)
+        
+        //self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+        self.fetchResult = albumColection
+    }
+    
+    private func prepareCollectionView() {
+            self.albumCollectionView.dataSource = self
+            self.albumCollectionView.delegate = self
+            self.albumCollectionView.register(UINib.init(nibName: "AlbumCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "AlbumCollectionViewCell")
+        }
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchResult.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: AlbumCollectionViewCell = albumCollectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! AlbumCollectionViewCell
+        
+        
+        return cell
+        
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+//
+//        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+//        //index에 해당하는 사진을
+//        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView?.image = image})
+//        //이미지 매니저를 통해 이미지를 요청함. 셀에 들어갈 사이즈로 작게, 가져온 뒤 셀에 이미지 넣어줌
+//
+//        return cell
+//    }
+    }
+    
+    func chexkAuthhorizationStatus(){
+        let photoAurhorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAurhorizationStatus {
+        case .authorized:
+            print("접근 허가")
+            self.requestCollection()
+            self.albumCollectionView.reloadData()
+        case .denied:
+            print("접근 불가")
+        case .notDetermined:
+            print("응답 없음")
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                switch status {
+                case .authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    
+                    OperationQueue.main.addOperation {
+                        self.albumCollectionView.reloadData()
+                    }
+                case .denied:
+                    print("사용자가 거절함")
+                default: break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        case .limited:
+            print("limited")
+        @unknown default:
+            fatalError()
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.prepareCollectionView()
     }
 
 
